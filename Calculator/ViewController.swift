b3d34c6afdf8313f8c76e534853519a573788417
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-08-13 13:30:53 -0300

    Adding new functionalities

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -19,6 +19,23 @@ class ViewController: UIViewController {
         
         //What should happen when a non-number button is pressed
         isFinishedTypingNumber = true
+        
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Cannot convert display label text to a Double")
+        }
+        
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-" {
+                displayLabel.text = String(number * -1)
+                
+            } else if calcMethod == "AC" {
+                displayLabel.text = "0"
+                
+            } else if calcMethod == "%" {
+                displayLabel.text = String( number / 100 )
+                
+            }
+        }
     }
 
     

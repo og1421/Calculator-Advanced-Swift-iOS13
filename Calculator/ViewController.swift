a92b0994fde627d6132680d4f98cd9bd976a1016
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-08-13 15:00:41 -0300

    Refactoring to computed properties

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,30 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     //MARK: - Properties
     @IBOutlet weak var displayLabel: UILabel!
     
     private var isFinishedTypingNumber: Bool = true
     
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display label text to a Double")
+            }
+            
+            return number
+        }
+        
+        set {
+            displayLabel.text = String(newValue)
+        }
+        
+    }
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
         isFinishedTypingNumber = true
         
-        guard let number = Double(displayLabel.text!) else {
-            fatalError("Cannot convert display label text to a Double")
-        }
+        
         
         if let calcMethod = sender.currentTitle {
             if calcMethod == "+/-" {
-                displayLabel.text = String(number * -1)
+                displayValue *=  -1
                 
             } else if calcMethod == "AC" {
                 displayLabel.text = "0"
                 
             } else if calcMethod == "%" {
-                displayLabel.text = String( number / 100 )
+                displayValue *= 0.01
                 
             }
         }
@@ -51,11 +64,8 @@ class ViewController: UIViewController {
             } else {
                 
                 if numValue == "." {
-                    guard let currentDisplayValue = Double(displayLabel.text!) else {
-                        fatalError("Cannot convert display label text to a double")
-                    }
                     
-                    let isInt = floor(Double(currentDisplayValue)) == Double(currentDisplayValue)
+                    let isInt = floor(Double(displayValue)) == displayValue
                     
                     if !isInt {
                         return
